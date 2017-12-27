7b95eaa47c19f78e3af4b593664f38c97e9dd93f
Aki <mj501738931@126.com>
2017-12-27 16:41:47 +0800

    Complete trans v2.1.0 collection file.

--- app/Commands/ConvertV21Command.php
+++ app/Commands/ConvertV21Command.php
@@ -33,6 +33,16 @@ class ConvertV21Command extends Command
     protected $version = 'v2.1.0';
 
     /**
+     * @var array
+     */
+    protected $headerTitle = ['key', 'value', 'description'];
+
+    /**
+     * @var array
+     */
+    protected $bodyTitle = ['key', 'type', 'value', 'description'];
+
+    /**
      * Execute the console command.
      *
      * @return mixed
@@ -44,35 +54,163 @@ class ConvertV21Command extends Command
         if ($this->getFileVersion($file) !== $this->version)
             $this->error('This postman collection is not version 2.1.0');
 
-        $info  = $this->getFileContent($file)['info'];
-        $items = collect($this->getFileContent($file)['item']);
+        $item = $this->getFileContent($file)['item'];
 
-        $content = $this->parseInfo($info);
+        $this->openFile();
 
-        $items->each(function ($item) use ($content) {
-            $this->line($item['name']);
-        });
+        $this->parseInfo($this->getFileContent($file)['info']);
+        $this->writeEnter();
+        $this->parseContents($item);
+        $this->writeEnter();
+        $this->parseItem($item);
 
-        $this->setOutputFile($this->argument('output'));
-
-        $this->append($content);
+        $this->closeFile();
     }
 
-    protected function parseInfo(array $info): string
+    /**
+     * @param array $info
+     */
+    protected function parseInfo(array $info): void
     {
         $title       = $info['name'];
-        $description = $info['description'];
+        $description = $info['description'] ?? '';
 
-        return "#{$title}\n$description\n\n\n";
+        $this->writeH($title);
+        $this->writeLine($description);
     }
 
-    protected function parseFolderItem(array $item): string
+    protected function parseContents(array $items, int $key = null)
     {
+        $key || $this->writeH('Contents', 2);
 
+        foreach ($items as $itemKey => $item) {
+            $h = isset($item['item']) ? 3 : 4;
+
+            $index = ($key ? "{$key}." : '') . ($itemKey + 1);
+            $this->writeAnchorLink($item['name'], $index, $h);
+
+            isset($item['item']) && $this->parseContents($item['item'], $itemKey + 1);
+        }
     }
 
-    protected function parseRequestItem(array $item): string
+    protected function parseItem(array $items): void
     {
+        $this->writeH('Body', 2);
 
+        foreach ($items as $key => $item) {
+            if (isset($item['item']))
+                $this->parseFolderItem($item, $key + 1);
+
+            elseif (isset($item['request']))
+                $this->parseRequestItem($item, $key + 1);
+        }
+    }
+
+    protected function parseFolderItem(array $folder, int $key): void
+    {
+        $title       = "{$key} {$folder['name']}";
+        $description = $folder['description'] ?? '';
+
+        $this->writeAnchor($title, $key, 3);
+        $this->writeLine($description);
+
+        if (isset($folder['item']))
+            foreach ($folder['item'] as $requestKey => $request) {
+                $requestKey++;
+                $this->parseRequestItem($request, $requestKey, $key);
+            }
+
+        $this->writeEnter();
+    }
+
+    protected function parseRequestItem(array $item, int $key, int $folderKey = null): void
+    {
+        $index       = ($folderKey ? "{$folderKey}." : '') . "{$key}";
+        $title       = "{$index} {$item['name']}";
+        $description = $item['request']['description'] ?? '';
+
+        $this->writeAnchor($title, $index, 4);
+        $this->writeLine($description);
+
+        //Request
+        $this->parseRequest($item['request']);
+
+        $this->writeEnter();
+    }
+
+    protected function parseRequest(array $request): void
+    {
+        //Url
+        $this->parseUrl($request['method'], $request['url']);
+
+        //Headers
+        $this->parseHeader($request['header']);
+
+        //Body
+        $this->parseBody($request['body']);
+    }
+
+    protected function parseUrl(string $method, array $url): void
+    {
+        $method = "{$method} ";
+        $url    = $url['raw'];
+
+        $this->writeH('URL', 5);
+        $this->writeCode($method);
+        $this->writeCode($url);
+        $this->writeEnter();
+    }
+
+    protected function parseHeader(array $header): void
+    {
+        if (count($header)) {
+            $this->writeH('HEADER', 5);
+            $this->writeTable($this->headerTitle, $header);
+        }
+    }
+
+    protected function parseBody(array $body): void
+    {
+        if (empty($body))
+            return;
+
+        switch ($body['mode']) {
+            case 'raw':
+                $raw     = $body['raw'];
+                $rawData = json_decode($raw, true);
+                $data    = [];
+
+                if (json_last_error() === 0) {
+                    foreach ($rawData as $key => $value) {
+                        $type = gettype($value);
+                        $type === 'array' && $value = json_encode($value);
+
+                        $data[] = [
+                            'key'   => $key,
+                            'type'  => $type,
+                            'value' => $value,
+                        ];
+                    }
+                }
+                break;
+            case 'formdata':
+                $data = $body['formdata'];
+                break;
+            case 'urlencoded':
+                $data = $body['urlencoded'];
+                break;
+            default:
+                $data = [];
+                break;
+        }
+
+        if (count($data)) {
+            $this->writeH('BODY', 5);
+            $this->writeTable($this->bodyTitle, $data);
+            if (isset($raw)) {
+                $this->writeH('RAW BODY', 5);
+                $this->writeCode($raw, true);
+            }
+        }
     }
 }
--- app/Commands/Traits/WriteMarkdown.php
+++ app/Commands/Traits/WriteMarkdown.php
@@ -8,7 +8,9 @@ trait WriteMarkdown
 {
     protected $outputFile = 'postman.markdown';
 
-    protected function setOutputFile(string $outputFile)
+    protected $content = '';
+
+    protected function setOutputFile(string $outputFile): void
     {
         $this->outputFile = $outputFile;
 
@@ -20,18 +22,144 @@ trait WriteMarkdown
         $this->createFile();
     }
 
-    protected function append(string $content): void
-    {
-        Storage::append($this->outputFile, $content);
-    }
-
-    protected function prepend(string $content): void
-    {
-        Storage::prepend($this->outputFile, $content);
-    }
-
     protected function createFile(): void
     {
         Storage::put($this->outputFile, '');
+    }
+
+    /**
+     * write h1, h2 etc.
+     * @param string $content
+     * @param int $h
+     */
+    protected function writeH(string $content, int $h = 1): void
+    {
+        if ($h < 0 || $h > 6)
+            $h = 1;
+
+        $this->writeLine(str_repeat('#', $h) . "{$content}");
+    }
+
+    /**
+     * Write anchor.
+     * @param string $content
+     * @param string|null $index
+     * @param int|null $h
+     */
+    protected function writeAnchor(string $content, string $index = null, int $h = null): void
+    {
+        $index = $index ?? $content;
+
+        $anchor = "<%s id=\"{$index}\">{$content}</%s>";
+
+        $label = (is_int($h) && $h > 0 && $h < 7) ? "h{$h}" : 'span';
+
+        $anchor = str_replace('%s', $label, $anchor);
+
+        $this->writeLine($anchor);
+    }
+
+    protected function writeAnchorLink(string $content, string $index, int $h = null): void
+    {
+        $url = "#{$index}";
+
+        $this->writeLink($content, $url, $h);
+    }
+
+    protected function writeLink(string $content, string $url, int $h = null): void
+    {
+        $link = "[{$content}]({$url})";
+
+        (is_int($h) && $h > 0 && $h < 7)
+            ? $this->writeH($link, $h)
+            : $this->writeLine($link, false);
+    }
+
+    /**
+     * @param array $headers
+     * @param array $rows
+     * @param string $align left,right,center
+     */
+    protected function writeTable(array $headers, array $rows, string $align = 'left'): void
+    {
+        $headerLine   = '|';
+        $dividingLine = '|';
+
+        foreach ($headers as $header) {
+            $headerLine .= " {$header} |";
+
+            if ($align === 'left' || $align === 'center')
+                $dividingLine .= ':';
+
+            $dividingLine .= "---";
+
+            if ($align === 'right' || $align === 'center')
+                $dividingLine .= ':';
+
+            $dividingLine .= '|';
+        }
+
+        unset($header);
+
+        $this->writeLine($headerLine);
+        $this->writeLine($dividingLine);
+
+        foreach ($rows as $row) {
+            $rowLine = '|';
+
+            foreach ($headers as $header)
+                $rowLine .= ' ' . ($row[$header] ?? '-') . ' |';
+
+            $this->writeLine($rowLine);
+        }
+    }
+
+    protected function writeCode(string $content, bool $multi = false): void
+    {
+        $codeSymbol = $multi ? '```' : '`';
+
+        $this->writeLine($codeSymbol, $multi);
+        $this->writeLine($content, $multi);
+        $this->writeLine("{$codeSymbol} ", $multi);
+    }
+
+    /**
+     * Write a line.
+     * @param string $content
+     * @param bool $enter
+     */
+    protected function writeLine(string $content = '', bool $enter = true): void
+    {
+        $this->write("{$content}");
+
+        $enter && $this->writeEnter();
+    }
+
+    /**
+     * Write a enter.
+     * @param int $lines
+     */
+    protected function writeEnter(int $lines = 1): void
+    {
+        $lines > 0 && $this->write(str_repeat("\n", $lines));
+    }
+
+    /**
+     * Write to file
+     * @param string $content
+     */
+    protected function write(string $content): void
+    {
+        $this->content .= $content;
+    }
+
+    protected function openFile(): void
+    {
+        $this->createFile();
+    }
+
+    protected function closeFile(): void
+    {
+        Storage::append($this->outputFile, $this->content, '');
     }
 }
